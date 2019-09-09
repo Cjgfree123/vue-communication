d9aaa8998dbf01d226bd09b0b9a2b22197902a31
jingechen <jingechen@sohu-inc.com>
2019-09-09 22:37:47 +0800

    add:init

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Parent></Parent>
+    </div>
+</template>
+
+<script>
+import Parent from "./components/Parent.vue";
+export default {
+    components:{
+        Parent,
+    },
+}
+</script>
+
---
+++ src/components/Parent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      parent
+      父组件 {{mny}}
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            mny:100,
+        }
+    }
+};
+</script>
